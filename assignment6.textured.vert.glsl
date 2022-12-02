8ee579971022192815a14d4ac21e88b9e15c4acd
Dylan Ang <dylanmouang@gmail.com>
2022-12-01 20:21:30 -0800

    Retrieve TBN matrix and perform gram-schmidt trick

--- assignment6.textured.vert.glsl
+++ assignment6.textured.vert.glsl
@@ -13,6 +13,10 @@ uniform mat4x4 u_p;
 
 // output to fragment stage
 // TODO: Create varyings to pass data to the fragment stage (position, texture coords, and more)
+out vec3 o_vertex_position_world;
+out vec3 o_vertex_normal_world;
+out vec2 o_texture_coord;
+out mat3 tbn;
 
 void main() {
 
@@ -21,13 +25,26 @@ void main() {
     // object space -{model}-> world space -{view}-> view space -{projection}-> clip space
     vec4 vertex_position_world = u_m * vec4(a_position, 1.0);
 
+    mat3 norm_matrix = transpose(inverse(mat3(u_m)));
+    vec3 vertex_normal_world = normalize(norm_matrix * a_normal);
+
     // TODO: Construct TBN matrix from normals, tangents and bitangents
     // TODO: Use the Gram-Schmidt process to re-orthogonalize tangents
     // NOTE: Different from the book, try to do all calculations in world space using the TBN to transform normals
     // HINT: Refer to https://learnopengl.com/Advanced-Lighting/Normal-Mapping for all above
-    mat3 tbn = mat3(0);
+    // transform to world space
+    vec3 T = normalize(vec3(u_m * vec4(a_tangent, 1.0)));
+    vec3 N = normalize(vec3(u_m * vec4(a_normal, 1.0)));
+    // re-orthogonalize T with respect to N - Gram Schmidt
+    T = normalize(T - dot(T, N) * N); 
+    // B is perpendicular to N and T
+    vec3 B = normalize(cross(N, T));
+    tbn = mat3(T, B, N);
 
     // TODO: Forward data to fragment stage
+    o_vertex_position_world = vertex_position_world.xyz;
+    o_vertex_normal_world = vertex_normal_world.xyz;
+
 
     gl_Position = u_p * u_v * vertex_position_world;
 
